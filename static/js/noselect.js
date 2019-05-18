93d31b0442535f95cb0871933dcdaabc713a67f5
Justin Van Patten <jvp@justinvp.com>
2019-05-18 15:29:13 -0700

    Fix noselect.js to work with chroma

--- static/js/noselect.js
+++ static/js/noselect.js
@@ -1,3 +1,8 @@
 $(function() {
-    $(".language-bash .nv:contains('$')").addClass("no-select");
+    // Make "$ " inside bash and shell code snippets unselectable.
+    var pattern = /((^|\r|\n|\r\n)( |\t)*\$ )/gi;
+    var replaceWith = '<span class="no-select">$1</span>';
+    $(".highlight .chroma .language-bash, .highlight .chroma .language-shell").each(function() {
+        $(this).html($(this).html().replace(pattern, replaceWith));
+    });
 });
